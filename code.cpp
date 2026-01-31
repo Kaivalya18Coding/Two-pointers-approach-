4ce6f8f6b06f0fe6d336da62bceb8cbf9175eb52
Kaivalya18Coding <rohit.mishra3005@gmail.com>
2026-01-31 16:07:28 +0530

    Two pointer approach

---
+++ code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int pointers (int arr[],int size) {
+    int start=0;
+    int end=size-1;
+    while(start<end) {
+        start++;
+        end--;
+    }
+
+
+}
+int main() {
+    int arr[]{ 5,7,9,4,6} ;
+    int size=5;
+    int pointer = pointers(arr,size);
+
+
+    
+
+
+
+}
